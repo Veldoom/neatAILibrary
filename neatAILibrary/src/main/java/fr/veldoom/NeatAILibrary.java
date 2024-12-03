4e80610a5b03e05ced6b33d9bbc6f3b6be1cfd65
Veldoom <130859974+Veldoom@users.noreply.github.com>
2024-12-03 18:52:02 +0100

    Add files via upload

---
+++ neatAILibrary/src/main/java/fr/veldoom/NeatAILibrary.java
@@ -0,0 +1,11 @@
+package fr.veldoom;
+
+public class NeatAILibrary {
+    public String testPublic() {
+        return "test public";
+    }
+
+    private String testPrivate() {
+        return "test private";
+    }
+}
