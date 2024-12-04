3703e12e400bb3b0d6b0f85307cfdc6b57eae4df
Veldoom <130859974+Veldoom@users.noreply.github.com>
2024-12-04 14:39:33 +0100

    Add files via upload

--- neatAILibrary/src/main/java/fr/veldoom/NeatAILibrary.java
+++ neatAILibrary/src/main/java/fr/veldoom/NeatAILibrary.java
@@ -1,11 +1,23 @@
 package fr.veldoom;
 
-public class NeatAILibrary {
-    public String testPublic() {
-        return "test public";
-    }
+import java.util.ArrayList;
 
-    private String testPrivate() {
-        return "test private";
+public class NeatAILibrary {
+    static int inputsNumber, outputsNumber;
+    static float mutationRate;
+    static ArrayList<Object> individualsList;
+    static ArrayList<Agent> agents;
+
+    public static void init(int inputsNumber, int outputsNumber, float mutationRate, ArrayList<Object> individualsList) {
+        NeatAILibrary.inputsNumber = inputsNumber;
+        NeatAILibrary.outputsNumber = outputsNumber;
+        NeatAILibrary.mutationRate = mutationRate;
+        NeatAILibrary.individualsList = individualsList;
+
+        agents = new ArrayList<>();
+
+        for (int i = 0; i != agents.size() - 1; i++) {
+            agents.add(new Agent(inputsNumber, outputsNumber));
+        }
     }
 }
